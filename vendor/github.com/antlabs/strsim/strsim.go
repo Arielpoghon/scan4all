57be71e6cf5220a8117f0af34ab68501d2c57df3
Ariel Poghon <mnangatariel27@gmail.com>
2026-09-25 10:26:22 +0300

    docs: translate string similarity comments

--- vendor/github.com/antlabs/strsim/strsim.go
+++ vendor/github.com/antlabs/strsim/strsim.go
@@ -4,7 +4,7 @@ import (
 	"github.com/antlabs/strsim/similarity"
 )
 
-// 比较两个字符串相似度
+// Compare returns the similarity of two strings.
 func Compare(s1, s2 string, opts ...Option) float64 {
 	var o option
 
@@ -13,13 +13,13 @@ func Compare(s1, s2 string, opts ...Option) float64 {
 	return compare(s1, s2, &o)
 }
 
-// 返回相似度最高的那个字符串
+// FindBestMatchOne returns the string with the highest similarity.
 func FindBestMatchOne(s string, targets []string, opts ...Option) *similarity.Match {
 	r := findBestMatch(s, targets, opts...)
 	return r.Match
 }
 
-// 返回相似度最高的那个字符串, 以及索引位置
+// FindBestMatch returns the string with the highest similarity and its index.
 func FindBestMatch(s string, targets []string, opts ...Option) *similarity.MatchResult {
 	return findBestMatch(s, targets, opts...)
 }
